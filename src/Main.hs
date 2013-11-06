f0e90c5865d7f4c53ffdfc95e1176b0617f7c8b0
School of Haskell <support@fpcomplete.com>
2013-11-06 12:56:17 +0000

    Add pointer to Tutorial.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE Arrows #-}
+-- This template is documented more completely in the tutorial at
+-- https://www.fpcomplete.com/school/advanced-haskell-1/xml-parsing-with-validation
+
 module Main where
 
 import Text.XML.HXT.Core
