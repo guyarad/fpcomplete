1c79c5e43d63de43232ff4d55cd9bcaec856ded4
School of Haskell <support@fpcomplete.com>
2013-11-06 12:29:34 +0000

    Initial version with valid data, etc.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,40 @@
--- | Main entry point to the application.
+{-# LANGUAGE Arrows #-}
 module Main where
 
--- | The main entry point.
+import Text.XML.HXT.Core
+import Text.XML.HXT.RelaxNG
+import Data.Fixed (Centi)
+import Data.Text (Text, pack)
+
+
+type Comment = Maybe Text
+data Transaction = Deposit !Text !Centi !Comment
+                 | Withdrawal !Text !Centi !Comment
+                 deriving Show
+
+make "deposit" = Deposit
+make "withdrawal" = Withdrawal
+make trans = error $ "Invalid transaction type: " ++ trans
+
+-- The select function should select the elements that are going to be processed.
+select = getChildren >>>  hasName "transactions" >>> getChildren >>> isElem
+
+-- The transform function transforms each element into a Haskell Data structure.
+transform = proc el -> do
+  trans <- getName -< el
+  account <- getAttrValue "account" -< el
+  amount <- getAttrValue "amount" -< el
+  comment <- withDefault (Just . pack ^<< getText <<< getChildren) Nothing -< el
+  returnA -< (make trans) (pack account) (read amount) comment
+
+-- The process function handles each Haskell data object in the XML file
+process = arrIO print
+
+-- The handleError function is called if there is an error in the input XML.
+handleError = getAttrValue a_source >>> arrIO (putStrLn . ("Error in document: " ++))
+
 main :: IO ()
 main = do
-    putStrLn "Welcome to FP Haskell Center!"
+  runX $ readDocument [withRelaxNG "transactions.rng"] "transactions.xml"
+         >>> ((select >>> transform >>> process) `orElse` handleError)
+  return ()
